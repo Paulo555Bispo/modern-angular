a85f15d7962b70a417154a87f461268c22310187
Paulo Bispo <63209799+Paulo555Bispo@users.noreply.github.com>
2026-06-19 22:19:00 -0300

    Criação de uma aplicação Angular V22, com um componente de cabeçalho usando Angular Material.

---
+++ src/app/header/header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatToolbar, MatButton, MatIcon],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {}
