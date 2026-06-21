0033cd210c227c1dc6f8e99d0035d94c75380086
Paulo Bispo <63209799+Paulo555Bispo@users.noreply.github.com>
2026-06-20 21:50:49 -0300

    Definimos uma interface, para segurança de tipos. Criamos uma interface de produtos. Criamos um signal, para armazenar a lista de produtos..

--- src/app/products/products-grid/products-grid.ts
+++ src/app/products/products-grid/products-grid.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductsCard } from '../products-card/products-card';
+import { Product } from '../product';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-products-grid',
-  imports: [ProductsCard],
+  imports: [ProductsCard, MatIconModule],
   templateUrl: './products-grid.html',
   styleUrl: './products-grid.scss',
 })
-export class ProductsGrid {}
+export class ProductsGrid {
+
+  protected readonly products = signal<Product[]>([
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'Description for product 1',
+      price: 19.99,
+      originalPrice: 29.99,
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      description: 'Description for product 2',
+      price: 9.99,
+    },
+    {
+      id: 3,
+      name: 'Product 3',
+      description: 'Description for product 3',
+      price: 14.99,
+      originalPrice: 19.99,
+    }
+  ]);
+}
