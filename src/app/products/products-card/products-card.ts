b42161f511294f5745b9ab5529bc593520df91a4
Paulo555Bispo <pbispo555@gmail.com>
2026-06-30 09:03:20 -0300

    Usamos @if e @else, para mostrar produtos com preços promocionais, caso tenha.

--- src/app/products/products-card/products-card.ts
+++ src/app/products/products-card/products-card.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { Product } from '../product';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-products-card',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, CurrencyPipe],
   templateUrl: './products-card.html',
   styleUrl: './products-card.scss',
 })
-export class ProductsCard {}
+export class ProductsCard {
+  readonly product = input.required<Product>();
+  readonly addButtonLabel = input('Add to cart');
+}
